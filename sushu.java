8f59fbf227035d56cc130e7126c76c6e81adeb73
Errors <62652629+Cleveland996@users.noreply.github.com>
2022-09-26 12:43:39 +0800

    Add files via upload

---
+++ sushu.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class sushu{
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        System.out.print("请输入一个大于二的整数：");
+        int usernum=input.nextInt();
+        int a,b,c;
+        for(a=2;a<=usernum;a++){
+            boolean pan=false;
+            b=1;
+            c=0;
+            while(b<=a){
+                    if(a%b==0) c++;
+                    b++;
+            }
+            if(c==2) System.out.print(a+"\t");
+        }
+    }
+}
