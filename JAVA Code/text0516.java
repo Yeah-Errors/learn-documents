f1723884877754d0113dbef090b178f3b4890364
Errors <62652629+EMO-Errors@users.noreply.github.com>
2022-10-18 23:14:01 +0800

    Add files via upload

---
+++ JAVA Code/text0516.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class text0516 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("请输入一个整数：");
+        int userNum = input.nextInt();
+        for(int i = 2; i <= userNum; i++){
+            if(userNum%i==0){
+                System.out.print(i+" ");
+                userNum /=i;
+                i=1;
+            }
+
+        }
+    }
+}
+
+
