42158e15a277587c7547e0e2e8461de36df9e988
Errors <62652629+EMO-Errors@users.noreply.github.com>
2022-10-22 01:24:29 +0800

    Add files via upload

---
+++ JAVA Code/Tasker.java
@@ -0,0 +1,14 @@
+public class Tasker {
+    public static void main(String[] args) {
+        int man, women, child, totalMoney = 50, totalPeople = 30 , ifAllChild = 30,i = 0;
+        for(man = 0; man <= totalMoney / 3 ; man++){
+            women = (totalMoney - (ifAllChild + 2 * man ));
+            child = totalMoney - women * 2 - man*3;
+            if(women + child +man == totalPeople && women > 0 && child > 0) {
+                System.out.printf("男人 %d 位，女人 %d 位，孩子 %d 位;\n",man,women,child);
+                i ++ ;
+            }
+        }
+        System.out.printf("共有以上 %d 种答案",i);
+    }
+}
---
+++ JAVA Code/Tasker2.java
@@ -0,0 +1,10 @@
+public class Tasker2 {
+    public static void main(String[] args) {
+        int chicken,rabbit,totalHead=35,totalFoot=94;
+        for(chicken = 0; chicken <= totalHead ; chicken++){
+            for(rabbit = 0; rabbit <= totalHead ; rabbit++ ){
+                if(chicken * 2 + rabbit * 4 == totalFoot && chicken + rabbit == totalHead) System.out.printf("共有 %d 只鸡， %d 只兔",chicken,rabbit);
+            }
+        }
+    }
+}
