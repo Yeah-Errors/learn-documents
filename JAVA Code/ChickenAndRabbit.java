242d32c0931f080e2750249a787daff0a9e4ee69
Errors <62652629+EMO-Errors@users.noreply.github.com>
2022-10-22 01:26:50 +0800

    Update and rename Tasker2.java to ChickenAndRabbit.java

--- JAVA Code/ChickenAndRabbit.java
+++ JAVA Code/ChickenAndRabbit.java
@@ -1,4 +1,4 @@
-public class Tasker2 {
+public class  ChickenAndRabbit {
     public static void main(String[] args) {
         int chicken,rabbit,totalHead=35,totalFoot=94;
         for(chicken = 0; chicken <= totalHead ; chicken++){
